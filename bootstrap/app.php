353b06674c13440f73dbc71698100b29c86c0d62
begimov <begimov@gmail.com>
2018-08-03 21:08:22 +0300

    eloquent orm set up done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,22 @@ $app = new Jenssegers\Lean\App();
 
 $container = $app->getContainer();
 
+$container->get('settings')->set('db', [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'jwtauthcmp',
+    'username'  => 'homestead',
+    'password'  => 'secret',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
 $container->get('settings')->set('displayErrorDetails', true);
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container->get('settings')->get('db'));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 require_once __DIR__ . '/../routes/web.php';
