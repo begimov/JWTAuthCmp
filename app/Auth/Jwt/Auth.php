9f0e2478ace4a2f5da9948640f9deafba9e622d2
begimov <begimov@gmail.com>
2018-08-05 10:07:19 +0300

    injecting AuthProviderInterface subtype in Auth class

--- app/Auth/Jwt/Auth.php
+++ app/Auth/Jwt/Auth.php
@@ -2,10 +2,23 @@
 
 namespace App\Auth\Jwt;
 
+use App\Auth\Providers\Auth\AuthProviderInterface;
+
 class Auth
 {
+    protected $auth;
+
+    public function __construct(AuthProviderInterface $auth)
+    {
+        $this->auth = $auth;    
+    }
+
     public function attempt($email, $password)
     {
+        if (!$user = $this->auth->byEmailAndPassword($email, $password)) {
+            return null;
+        }
+
         return 'token';
     }
 }
