c5afc99dbc8cdd9af63b96059690ac46cf4364e5
begimov <begimov@gmail.com>
2018-08-05 08:23:25 +0300

    post login web route created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\Auth\LoginController;
 
 $app->get('/', HomeController::class . ':index');
+
+$app->post('/auth/login', LoginController::class . ':index');
