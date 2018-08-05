3dd9226b0cee2f8c9d674b6f381bbd222f522d5d
begimov <begimov@gmail.com>
2018-08-05 11:25:17 +0300

    jwt factory updated to chain on methods after passing in claims

--- app/Auth/Jwt/Factory.php
+++ app/Auth/Jwt/Factory.php
@@ -4,9 +4,18 @@ namespace App\Auth\Jwt;
 
 class Factory
 {
+    protected $claims = [];
+
+    public function withClaims(array $claims)
+    {
+        $this->claims = $claims;
+        
+        return $this;
+    }
+
     public function make()
     {
-        //
+        return $this->claims;
     }
 
     public function encode()
