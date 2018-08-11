3d4ab0be0ab392b3c4710ada0bea248af04832db
begimov <begimov@gmail.com>
2018-08-11 13:01:12 +0300

    minor refactoring done to web routes file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Auth\Jwt\Auth;
+use App\Middleware\Authenticate;
 use App\Controllers\HomeController;
 use App\Controllers\Auth\LoginController;
 
-$app->get('/', HomeController::class . ':index')->add(new \App\Middleware\Authenticate($container->get(App\Auth\Jwt\Auth::class)));
+$app->get('/', HomeController::class . ':index')
+    ->add(new Authenticate($container->get(Auth::class)));
 
 $app->post('/auth/login', LoginController::class . ':login');
