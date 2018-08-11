60bc8488d74c29b1f28058a23d813f64f7b63b26
begimov <begimov@gmail.com>
2018-08-11 12:52:19 +0300

    Jwt Auth pulled in Authenticate middleware

--- app/Middleware/Authenticate.php
+++ app/Middleware/Authenticate.php
@@ -2,15 +2,29 @@
 
 namespace App\Middleware;
 
+use App\Auth\Jwt\Auth;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class Authenticate
 {
+    protected $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     public function __invoke(Request $request, Response $response, $next)
     {
         if (!$header = $this->getAuthorizationHeader($request)) {
             return $response->withStatus(401);
+        }
+
+        try {
+            $this->auth->authenticate($header);
+        } catch(\Exception $e) {
+            // respond with 401 message
         }
 
         return $next($request, $response);
