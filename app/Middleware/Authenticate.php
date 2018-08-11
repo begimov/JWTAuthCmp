33514f31dc0c70afc7af977b9eddf0a35a566693
begimov <begimov@gmail.com>
2018-08-11 12:43:48 +0300

    getting Auth header implemented in Authenticate middleware

--- app/Middleware/Authenticate.php
+++ app/Middleware/Authenticate.php
@@ -9,6 +9,19 @@ class Authenticate
 {
     public function __invoke(Request $request, Response $response, $next)
     {
+        if (!$header = $this->getAuthorizationHeader($request)) {
+            return $response->withStatus(401);
+        }
+
         return $next($request, $response);
+    }
+
+    protected function getAuthorizationHeader($request)
+    {
+        if (!list($header) = $request->getHeader('Authorization', false)) {
+            return false;
+        }
+
+        return $header;
     }
 }
